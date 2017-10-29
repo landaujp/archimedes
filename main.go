d479fead27e2b9fecc3e9836ae958d3941d8f22e
egapool <egpersent@gmail.com>
2017-10-29 17:52:49 +0900

    gofmaもれ

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"net/http"
-	"encoding/json"
-	"strconv"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"encoding/json"
+	"fmt"
 	"github.com/BurntSushi/toml"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"strconv"
+	"time"
 )
+
 type Config struct {
-    DB struct {
-    	User string
-    	Password string
-    	Port int
-    }
+	DB struct {
+		User     string
+		Password string
+		Port     int
+	}
 }
 
 type Jsondata struct {
 	Last, Bid, Ask, High, Low, Volume float32
-	Timestamp int
+	Timestamp                         int
 }
 
 func main() {
 	var config Config
 	_, err := toml.DecodeFile("config.toml", &config)
 	if err != nil {
-	    // Error Handling
+		// Error Handling
 	}
 	db, err := sql.Open("mysql", config.DB.User+":"+config.DB.Password+"@tcp(127.0.0.1:"+strconv.Itoa(config.DB.Port)+")/market")
 	if err != nil {
@@ -45,14 +46,14 @@ func main() {
 			return
 		}
 		if resp.StatusCode != 200 {
-			continue;
+			continue
 		}
 
 		data := Jsondata{}
 		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
 			panic(err)
 		}
-		_, err = db.Exec("INSERT INTO coincheck (last,created_at) VALUES (?,?)", data.Last,time.Now())
+		_, err = db.Exec("INSERT INTO coincheck (last,created_at) VALUES (?,?)", data.Last, time.Now())
 		if err != nil {
 			panic(err.Error())
 		}
